4cf265c2e26f10fbbb9777dbff58132c89d92bd1
ADLANI Asma <asma@Ordinateur-portable-de-ADLANI.local>
2025-07-22 21:15:13 +0200

    Correction workflow GitHub Actions: utilisation de gh-pages avec peaceiris/actions-gh-pages

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,9 +1,8 @@
 "use client"
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes/dist/types"
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: { children: React.ReactNode; [key: string]: any }) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
